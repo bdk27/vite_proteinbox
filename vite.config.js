6808ac4272ca3088a41b18daa7da21aa4b51d829
BDK27 <briansam195@gmail.com>
2023-01-10 23:35:11 +0800

    add  requireTransform

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import requireTransform from 'vite-plugin-require-transform';
 
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/vite_proteinbox/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    requireTransform.default({
+      fileRegex: /.js$|.vue$/,
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
